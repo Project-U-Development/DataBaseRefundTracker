68421eaad63bbbfe5bf013949282578506f9532d
vikawal <108365967+vikawal@users.noreply.github.com>
2022-12-18 19:29:52 +0100

    old file no tables in database

--- db_refundTracker.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE RefundTracker;
-SHOW DATABASES;
-use RefundTracker;
-CREATE TABLE users (user_id INT PRIMARY KEY, user_name VARCHAR(40));
-INSERT INTO users VALUES (1, 'Jack');
-INSERT INTO users VALUES (2, 'Ann');
-SHOW TABLES;
-SHOW FULL TABLES;
-DESCRIBE RefundTracker.users;
