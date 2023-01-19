1bf8718196fdd53056aefc61c3addbb9e1293065
vikawal <108365967+vikawal@users.noreply.github.com>
2023-01-19 21:31:57 +0100

    Update db_refundTracker.sql

--- db_refundTracker.sql
+++ db_refundTracker.sql
@@ -23,29 +23,29 @@ amount$ DECIMAL(10,2),
 currency VARCHAR(3), 
 due_date DATE, 
 user_id VARCHAR(50) NOT NULL, 
-if_completed BOOLEAN, 
+if_completed ENUM('yes', 'no', 'deleted'), 
 FOREIGN  KEY (user_id) REFERENCES users(user_id)
 );
 CREATE TABLE reminder (
 reminder_id VARCHAR(50), PRIMARY KEY,
-reminder_type VARCHAR (7),
+reminder_type ENUM('each', 'every'),
 frequency INT,
-time_unit VARCHAR (10),
+time_unit ENUM('week(s)', 'day(s)'),
 refund_id INT,
 FOREIGN KEY (refund_id) REFERENCES refunds (refund_id)
 );
 DESCRIBE reminder;
 DESCRIBE refunds;
-INSERT INTO refunds VALUES (1, '2022-01-01 12:47:53', 'shoes', 'Nike', 20.99, 'euro', '2024-06-01', '3', 0);
-INSERT INTO refunds VALUES (2, '2022-01-02 23:59:59', 'dress', 'Dresscompany', 17.50, 'usd', '2024-05-02', '1', 0);
-INSERT INTO refunds VALUES (3, '2022-01-03 09:12:05', 'socks', 'madeByMom', 7.45, 'cad', '2024-09-03', '2', 0);
-INSERT INTO refunds VALUES (4, '2022-01-04 13:08:27', 'bedlinen', 'SleepNice', 75.80, 'euro', '2024-06-17','6', 0);
+INSERT INTO refunds VALUES (1, '2022-01-01 12:47:53', 'shoes', 'Nike', 20.99, 'euro', '2024-06-01', '3', 'no');
+INSERT INTO refunds VALUES (2, '2022-01-02 23:59:59', 'dress', 'Dresscompany', 17.50, 'usd', '2024-05-02', '1', 'no');
+INSERT INTO refunds VALUES (3, '2022-01-03 09:12:05', 'socks', 'madeByMom', 7.45, 'cad', '2024-09-03', '2', 'no');
+INSERT INTO refunds VALUES (4, '2022-01-04 13:08:27', 'bedlinen', 'SleepNice', 75.80, 'euro', '2024-06-17','6', 'no');
 INSERT INTO refunds VALUES (5, '2022-02-19 14:15:08', 'mobile', 'Apple', 1299.90, 'usd', '2024-03-23', '1', 0);
-INSERT INTO refunds VALUES (6, '2022-03-26 15:37:41', 'flightMadrid', 'Wizzair', 578.20, 'euro', '2024-04-23', '5', 0);
-INSERT INTO refunds VALUES (7, '2022-03-28 16:41:23', 'hummer', 'KitchenCo', 255.90, 'uah', '2024-07-15', '4', 0);
-INSERT INTO refunds VALUES (8, '2022-06-10 17:03:19', 'pillow', 'SleepNice', 45.20, 'euro', '2024-02-13', '7', 0);
-INSERT INTO refunds VALUES (9, '2022-04-17 18:19:34', 'jacket', 'Jacketcompany', 85.99, 'usd', '2024-04-25','8', 0);
-INSERT INTO refunds VALUES (10, '2022-04-09 19:47:29', 'jeans', 'Dresscompany', 34.00, 'usd', '2024-05-16','9', 0);
+INSERT INTO refunds VALUES (6, '2022-03-26 15:37:41', 'flightMadrid', 'Wizzair', 578.20, 'euro', '2024-04-23', '5', 'no');
+INSERT INTO refunds VALUES (7, '2022-03-28 16:41:23', 'hummer', 'KitchenCo', 255.90, 'uah', '2024-07-15', '4', 'no');
+INSERT INTO refunds VALUES (8, '2022-06-10 17:03:19', 'pillow', 'SleepNice', 45.20, 'euro', '2024-02-13', '7', 'no');
+INSERT INTO refunds VALUES (9, '2022-04-17 18:19:34', 'jacket', 'Jacketcompany', 85.99, 'usd', '2024-04-25','8', 'no');
+INSERT INTO refunds VALUES (10, '2022-04-09 19:47:29', 'jeans', 'Dresscompany', 34.00, 'usd', '2024-05-16','9', 'no');
 INSERT INTO reminder VALUES (1, 'each', 2, 'days', 9);
 INSERT INTO reminder VALUES (2, 'every', 1, 'week', 8);
 INSERT INTO reminder VALUES (3, 'every', 5, 'days', 7);
